b39939253c9bfbe1e7fd68294d68b977737bd64a
ltpoole <laborris.poole@gmail.com>
2023-12-05 19:30:00 -0500

    created NamesList component and rendered 4 NameBadge components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import NameBadge from "./components/NameBadge/NameBadge";
+import NamesList from "./components/NamesList/NamesList";
 
 function App() {
   return (
     <div className="App">
       <NameBadge displayedName="LaBorris Poole" />
+      <NamesList />
     </div>
   );
 }
---
+++ src/components/NamesList/NamesList.jsx
@@ -0,0 +1,12 @@
+const NamesList = ({}) => {
+  return (
+    <div>
+      <h3>Vickie</h3>
+      <h3>Charity</h3>
+      <h3>Aiden</h3>
+      <h3>Aubrey</h3>
+    </div>
+  );
+};
+
+export default NamesList;
